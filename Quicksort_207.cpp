f4c63f805e876ead7bcf3db48ee50fa7ce1a08d7
shabit24 <hossain.shabit24@gmail.com>
2023-03-31 10:58:54 +0700

    Add project files.

---
+++ Quicksort_207.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+// array integers to hold values
+int arr[20];
+int cmp_count = 0;
+int n;
+void input() {
+    while (true) 
+    {
+        cout << "Masukan panjang array :";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\n Masukan penjnag array 20"
+    }
+}
+
